1876b90f1d32d76b808a193e9662b07199552bec
Siarhei Kazakevich <dianrom@tut.by>
2024-12-26 14:37:59 +0300

    refactor: hello my ds-task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -3,7 +3,7 @@ const { NotImplementedError } = require("../extensions/index.js");
 // const { Node } = require('../extensions/list-tree.js');
 
 /**
- * Implement simple binary search tree according to task description
+ * Implement simple binary search tree according to task description.
  * using Node from extensions
  */
 
